1c08eb416e174f4a01ae1da74264417494bad33d
manohar tanna <manohartanna137@gmail.com>
2023-07-09 19:34:28 +0530

    added a code related to map with a condition and added print statement

--- git_demo_python.py
+++ git_demo_python.py
@@ -1,5 +1,5 @@
 from functools import reduce
-list1=[10,20,30,40]
+list1=[10,20,30,40,55,65,88]
 addition=reduce(lambda x,y : x+y,list1)
 print(addition)
 
@@ -9,4 +9,9 @@ print(add_three)
 print("end")
 for x in range(1,10):
     print(i)
+
+condition=list(map(lambda x : x if x%2==0 else 1,list1))
+print(condition)
+
+print("Hello all ,welcome to the session")
 
